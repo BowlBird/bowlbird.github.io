043768104b17df59341ee72d665c5e778d75e961
BowlBird <millercarson111@gmail.com>
2024-02-11 23:24:04 -0600

    implemented text input

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,54 @@
-// import Image from "next/image";
+'use client';
+
+import { Textarea, input } from '@nextui-org/react';
+import {JetBrains_Mono} from 'next/font/google'
+import { useEffect, useState } from 'react';
+
+const jetbrainsMono = JetBrains_Mono({subsets: ['latin']})
+
 
 export default function Home() {
+  const [output, setOutput] = useState([] as string[])
+  const [inputText, setInputText] = useState('')
+  /**
+   * Globally check for input, and add it to text area.
+   * if Enter is pressed, send command.
+   */
+  useEffect(() => {
+    const callback = (e: KeyboardEvent) => {
+      if (e.key) {
+        document.getElementById('input')?.focus()
+      }
+    }
+    window.addEventListener('keydown', callback)
+  
+    return () => window.removeEventListener('keydown', callback);
+  }, [])
+  
   return (
-  <main>
-    <h1>BowlBird Website :)</h1>
-    <p>with an autodeploy test</p>
-  </main>
+    <div className={`${jetbrainsMono.className} m-1`}>
+      {output.map((it, i) => (<p key={i}>{it}</p>))}
+      <div className='flex'>
+        <Textarea 
+          value={inputText}
+
+          onValueChange={(character) => {
+            const newChar = character.substring(inputText.length)
+            if (newChar != '\n') setInputText(character)
+            else {
+              setInputText('')
+              setOutput([...output, '$ ' + inputText, inputText])
+            }
+          }}
+          spellCheck='false'
+          className='w-full'
+          id='input'
+          type='text'
+          label='$'
+          labelPlacement='outside'
+        />
+      </div>
+    </div>
   );
 }
+
